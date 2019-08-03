27c326cf941e0bd93867b21a768c7fa68629a2df
jch1902 <jasper.d39c@gmail.com>
2019-08-03 15:47:38 -0700

    Nevigation

--- app/src/main/java/com/ucsdextandroid2/android2final/ListActivity.java
+++ app/src/main/java/com/ucsdextandroid2/android2final/ListActivity.java
@@ -10,8 +10,12 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.List;
 
@@ -21,57 +25,71 @@ public class ListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.recycleview_list);
+        //setContentView(R.layout.recycleview_list);
 
-        listAdapter = new ListAdapter();
-
-        RecyclerView recyclerView =findViewById(R.id.rv_list);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(listAdapter);
-
-        getAPIdata();
-
-        listAdapter.setClickListener(new OnItemClickListener<Park>() {
-            @Override
-            public void onItemClicked(Park item) {
-                Intent intent = new Intent(ListActivity.this, ParkDetailActivity.class);
-                intent.putExtra("park", item);
-
-                startActivity(intent);
-
-                ParkEntity parkEntity = new ParkEntity();
-                parkEntity.id = item.getId();
-                parkEntity.name = item.getName();
-                parkEntity.imageUrl = item.getImages().get(0).getUrl();
-
-                // add to database
-                addParktoDB(parkEntity);
-            }
-        });
+        setContentView(R.layout.activity_park);
     }
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
-        return super.onCreateView(name, context, attrs);
-    }
+    private void setUpBottomNavBar() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.ai_bottom_nav_view);
 
+        NavController navController = Navigation.findNavController(this, R.id.ai_nav_host);
 
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
-    private void getAPIdata(){
-        DataSources.getInstance().getParks(new DataSources.Callback<List<Park>>() {
-            @Override
-            public void onDataFetched(List<Park> data) {
-                Log.d("ListActivity", "API returns Park: "+ data.size());
-                listAdapter.setList(data);
-            }
-        });
+        //AlternativeNavigationUI.setupWithNavController(bottomNavigationView, navController);
+
 
     }
-
-
-    private void addParktoDB(ParkEntity parkEntity){
-            AppDatabase.getAppDatabase(this).parkDao().insert(parkEntity);
-    }
+//        listAdapter = new ListAdapter();
+//
+//        RecyclerView recyclerView =findViewById(R.id.rv_list);
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+//        recyclerView.setLayoutManager(layoutManager);
+//        recyclerView.setAdapter(listAdapter);
+//
+//        getAPIdata();
+//
+//        listAdapter.setClickListener(new OnItemClickListener<Park>() {
+//            @Override
+//            public void onItemClicked(Park item) {
+//                Intent intent = new Intent(ListActivity.this, ParkDetailActivity.class);
+//                intent.putExtra("park", item);
+//
+//                startActivity(intent);
+//
+//                ParkEntity parkEntity = new ParkEntity();
+//                parkEntity.id = item.getId();
+//                parkEntity.name = item.getName();
+//                parkEntity.imageUrl = item.getImages().get(0).getUrl();
+//
+//                // add to database
+//                addParktoDB(parkEntity);
+//            }
+//        });
+//    }
+//
+//    @Nullable
+//    @Override
+//    public View onCreateView(@NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
+//        return super.onCreateView(name, context, attrs);
+//    }
+//
+//
+//
+//    private void getAPIdata(){
+//        DataSources.getInstance().getParks(new DataSources.Callback<List<Park>>() {
+//            @Override
+//            public void onDataFetched(List<Park> data) {
+//                Log.d("ListActivity", "API returns Park: "+ data.size());
+//                listAdapter.setList(data);
+//            }
+//        });
+//
+//    }
+//
+//
+//    private void addParktoDB(ParkEntity parkEntity){
+//            AppDatabase.getAppDatabase(this).parkDao().insert(parkEntity);
+//    }
 }
--- app/src/main/java/com/ucsdextandroid2/android2final/ListFragment.java
+++ app/src/main/java/com/ucsdextandroid2/android2final/ListFragment.java
@@ -27,7 +27,7 @@ public class ListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_main, container, false);
+        return inflater.inflate(R.layout.recycleview_list, container, false);
     }
 
     @Override
